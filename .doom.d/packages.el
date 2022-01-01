a8d8679153569d18d44933fcfa1fb4351d0fd4e4
Ian Waudby-Smith <5490890+WannabeSmith@users.noreply.github.com>
2022-01-01 12:59:11 -0500

    Use numpydoc

--- .doom.d/packages.el
+++ .doom.d/packages.el
@@ -10,6 +10,8 @@
 
 (package! jupyter)
 
+(package! numpydoc)
+
 ;; To install a package directly from a remote git repo, you must specify a
 ;; `:recipe'. You'll find documentation on what `:recipe' accepts here:
 ;; https://github.com/raxod502/straight.el#the-recipe-format
